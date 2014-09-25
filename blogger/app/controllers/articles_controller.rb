64294bf6bb14ff44185ff005dfdd34dad71a18eb
metalheaddev <sean.ivins@yahoo.com>
2014-09-24 20:43:51 -0500

    Finished everything up for tonight

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -8,4 +8,18 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def new
+		@article = Article.new		
+	end
+
+	def create
+		@article = Article.create(article_params)
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
 end
